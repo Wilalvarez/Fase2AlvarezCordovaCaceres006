4183f8073106acec1d4ba38791797f2958196778
unknown <WillBrael@gmail.com>
2020-10-20 09:44:01 -0300

    WidiaTours: Creando Modelos

--- widiatours/catalogo/models.py
+++ widiatours/catalogo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse #redirecciona una url de un producto al browser
+import uuid                     #se utiliza para definir atributos clave (pk)
 
 # Create your models here.
+
+class Compra(models.Model):
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Codigo unico del producto')
+    name=models.CharField(max_length=100)
+    precio=models.CharField('PRECIO', max_length=13)
+    descripcion=models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return f'{self.id} ({self.name}) ({self.descripcion})'
