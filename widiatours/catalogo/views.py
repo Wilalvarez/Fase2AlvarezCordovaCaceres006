53b132c2391a0c90a6298d9ed16553df4f92b099
unknown <WillBrael@gmail.com>
2020-10-27 12:28:41 -0300

    Widiatours: Creando Página de Inicio

--- widiatours/catalogo/views.py
+++ widiatours/catalogo/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Compra
 
-# Create your views here.
+
+def index(request):
+    num_compras=Compra.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_compras':num_compras},
+    )
+
+def acercadeee(request):
+
+    return render(
+        request,
+        'acercadeee.html',
+    )
+
